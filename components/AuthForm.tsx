77f5a306cd1b6e3a236c3658dc9e186d0ab759ef
seansuen90 <seansuen90@gmail.com>
2026-02-12 08:37:15 +0000

    impl auth forms

---
+++ components/AuthForm.tsx
@@ -0,0 +1,37 @@
+'use client'
+
+import Link from 'next/link'
+import Image from 'next/image'
+import React, { useState } from 'react'
+
+
+const AuthForm = ({type}: {type: string}) => {
+const [user, setUser] = useState(null)
+  return (
+    <section className='auth-form'>
+        <header className='flex flex-col gap-5 md:gap-8'>
+            <Link href='/' className="cursor-pointer flex items-center gap-1">
+                <Image 
+                    src='/icons/logo.svg'
+                    width={34}
+                    height={34} 
+                    alt='Horizon Logo'
+                />
+                <h1 className='text-26 font-ibm-plex-serif font-bold text-black-1'>Horizon</h1>
+            </Link>
+            <div className='flex flex-col gap-1 md:gap-3'>
+                <h1>
+                    {user
+                        ? 'Link Account'
+                        : type === 'sign-in'
+                            ? 'Sign In'
+                            : 'Sign Up'
+                    }
+                </h1>
+            </div>
+        </header>
+    </section>
+  )
+}
+
+export default AuthForm

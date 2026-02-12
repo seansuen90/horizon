ed7cff85b5756a251fe0e2ad7c078ee20243b734
seansuen90 <seansuen90@gmail.com>
2026-02-12 11:32:05 +0000

    building auth form

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -20,7 +20,7 @@ const [user, setUser] = useState(null)
                 <h1 className='text-26 font-ibm-plex-serif font-bold text-black-1'>Horizon</h1>
             </Link>
             <div className='flex flex-col gap-1 md:gap-3'>
-                <h1>
+                <h1 className='text-24 lg:text-36 font-semibold text-gray-900'>
                     {user
                         ? 'Link Account'
                         : type === 'sign-in'
@@ -28,8 +28,23 @@ const [user, setUser] = useState(null)
                             : 'Sign Up'
                     }
                 </h1>
+                <p className='text-16 font-normal text-gray-600'>
+                    {user 
+                        ? 'Link your account to get started'
+                        : 'Please enter your details'
+                    }
+                </p>
             </div>
         </header>
+        {user ? (
+            <div className='flex flex-col gap-4'>
+                {/* PlaidLink */}
+            </div>
+        ): (
+            <>
+            Form
+            </>
+        )}
     </section>
   )
 }
